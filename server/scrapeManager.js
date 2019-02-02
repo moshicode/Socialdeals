983f354a75072cf560086c26d788ed736ca39d87
Moshiko <moshikoshamir@live.com>
2019-02-02 16:11:18 +0200

    clean code

--- server/scrapeManager.js
+++ server/scrapeManager.js
@@ -5,25 +5,12 @@ const amazon = require('./scrape-modules/amazon-com');
 module.exports = {
     "aliexpress": aliexpress,
     "ebay": ebay,
-    "amazon": amazon,
+    "amazon": amazon
+    // "geekbuying": geekbuying,
+    // "banggood": banggood,
+    // "gearbest": gearbest,
+    // "asos": asos,
+    // "joybuy": joybuy
+    // "other": notFound
+
 };
-
-
-// module.exports = {
-//     aliexpress: getScrape
-// }
-
-// // class Url {
-// //     constructor(url) {
-// //         this.url = url
-// //     }
-
-// //     extractUrl() {
-// //         let sourceString = url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0];
-// //         console.log(extractUrl)
-// //     }
-// // }
-
-// // const cat = new Url("https://www.aliexpress.com/item/Fashion-seller-Old-World-Map-mouse-pad-2016-new-large-pad-to-mouse-notbook-computer-mousepad/32727458482.html")
-
-// // module.exports = Url
