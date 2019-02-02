2249c4a01498b2d9c4407b36369ee32d832552c3
Moshiko <moshikoshamir@live.com>
2019-02-02 17:35:38 +0200

    set new sites

--- server/scrapeManager.js
+++ server/scrapeManager.js
@@ -1,15 +1,19 @@
 const aliexpress = require('./scrape-modules/aliexpress-com');
 const ebay = require('./scrape-modules/ebay-com');
 const amazon = require('./scrape-modules/amazon-com');
+const banggood = require('./scrape-modules/banggood-com');
+const asos = require('./scrape-modules/asos-com');
+
+
 
 module.exports = {
     "aliexpress": aliexpress,
     "ebay": ebay,
-    "amazon": amazon
+    "amazon": amazon,
     // "geekbuying": geekbuying,
-    // "banggood": banggood,
+    "banggood": banggood,
     // "gearbest": gearbest,
-    // "asos": asos,
+    "asos": asos
     // "joybuy": joybuy
     // "other": notFound
 
