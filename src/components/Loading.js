12024f634352913dbb28c8ae06320fbefd2c5a6c
Moshiko <moshikoshamir@live.com>
2019-02-03 13:50:59 +0200

    fix loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="spinner-block">
+        <div className="spinner-wrapper">
             <div className="spinner">
                 <div className="double-bounce1"></div>
                 <div className="double-bounce2"></div>
