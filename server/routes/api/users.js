ceb4875e838622893aa8959936f4194279c97adf
Moshiko <moshikoshamir@live.com>
2019-02-04 17:32:29 +0200

    set simple register and login route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,9 +3,46 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/User')
 
-
-router.post('/register', (req, res) => {
-
+// All users
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        res.send(users)
+    })
 })
+
+// Register User
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists' })
+            } else {
+                const newUser = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                newUser.save()
+                res.end()
+            }
+        })
+})
+
+
+router.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({ username, password })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ username: 'User not found' })
+            } else {
+                return res.status(200).send()
+            }
+        })
+})
+
+
 
 module.exports = router;
