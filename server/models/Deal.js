69ac0b2056b6fac56c449dc67e96315c61289ff5
Moshiko <moshikoshamir@live.com>
2019-02-04 21:55:25 +0200

    set new schema for deal

--- server/models/Deal.js
+++ server/models/Deal.js
@@ -3,9 +3,16 @@ const Schema = mongoose.Schema
 
 // Create Deal Schema
 const DealSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
     date: {
         type: Date,
         default: Date.now
+    },
+    username: {
+        type: String
     },
     title: {
         type: String,
@@ -35,7 +42,31 @@ const DealSchema = new Schema({
     },
     dealEndDate: {
         type: Date
-    }
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
 })
 
 const Deal = mongoose.model("deal", DealSchema)
