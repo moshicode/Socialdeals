a2319492acf96f982b0747cb9a96f01961bebeb0
moshicode <moshikoshamir@live.com>
2019-02-11 20:25:52 +0200

    try deploy

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialdeals');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'client/src')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(bodyParser.json())
@@ -24,8 +23,10 @@ app.use('/api/users', users)
 app.use('/api/scrape', scrape)
 app.use('/api/currencies', currencies)
 
+app.use(express.static('client/build'))
+
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 const port = process.env.PORT || 5000
 
